f48d8ef16e1e038cd7f747ee8afc107f9e14803f
inept <azer.taboubi@issatso.u-sousse.tn>
2020-03-28 02:56:27 +0100

    Final Stage 3

--- Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/task/src/editor/TextEditor.java
@@ -5,12 +5,18 @@ import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+
 interface SaveLoadListener {
     enum Actions {
         SAVE_FILE,
@@ -33,15 +39,81 @@ public class TextEditor extends JFrame implements SaveLoadListener {
         scrollableTextArea.setName("ScrollPane");
         scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
         BorderFactory.setMargin(scrollableTextArea, 10, 10, 10, 10);
         add(scrollableTextArea, BorderLayout.CENTER);
         SaveLoadControl fileSelection = new SaveLoadControl();
         fileSelection.addSaveLoadListener(this);
         BorderFactory.setMargin(fileSelection, 5, 0, 0, 5);
         add(fileSelection, BorderLayout.NORTH);
+
         setVisible(true);
 
-    }
+
+        JMenuBar menuBar;
+        JMenu menu;
+        JMenuItem menuItem;
+
+//Create the menu bar.
+        menuBar = new JMenuBar();
+
+//Build the first menu.
+        menu = new JMenu("Menu");
+        menu.setName("MenuFile") ;
+        menu.setMnemonic(KeyEvent.VK_A);
+      //  menu.getAccessibleContext().setAccessibleDescription("The only menu in this program that has menu items");
+        menuBar.add(menu);
+
+//a group of JMenuItems
+
+
+        //item Load
+        menuItem = new JMenuItem("Load", KeyEvent.VK_T);
+        menuItem.setName("MenuLoad");
+        menuItem.setMnemonic(KeyEvent.VK_B);
+        menu.add(menuItem);
+        menuItem.addActionListener(actionEvent -> {
+            try {
+                fileSelection.loadFileButtonAction();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        //item save
+        menuItem = new JMenuItem("Save");
+        menuItem.setName("MenuSave");
+        menuItem.setMnemonic(KeyEvent.VK_B);
+        menu.add(menuItem);
+        menuItem.addActionListener(actionEvent -> {
+            try {
+                fileSelection.saveFileButtonAction();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        //Item exit
+        menuItem = new JMenuItem("Exit");
+        menuItem.setName("MenuExit");
+        menuItem.setMnemonic(KeyEvent.VK_D);
+        menu.add(menuItem);
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                dispose() ;
+            }
+        });
+
+
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+        validate();
+        repaint();
+
+
+        }
+
 
     @Override
     public void onClick(String fileName, Actions action) throws IOException {
@@ -117,7 +189,7 @@ class SaveLoadControl extends JPanel {
         buttonListeners.add(listener);
     }
 
-    private void loadFileButtonAction() throws IOException {
+    public void loadFileButtonAction() throws IOException {
         String fileName = fileNameField.getText();
         if (fileName.isEmpty()) {
             JOptionPane.showMessageDialog(null, "File name is empty!");
@@ -128,7 +200,7 @@ class SaveLoadControl extends JPanel {
         }
     }
 
-    private void saveFileButtonAction() throws IOException {
+    public void saveFileButtonAction() throws IOException {
         String fileName = fileNameField.getText();
         if (fileName.isEmpty()) {
             JOptionPane.showMessageDialog(null, "File name is empty!");
